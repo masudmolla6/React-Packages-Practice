d7a102939c3f8021f22e32441026e8a452c0e506
masudmolla6 <masudmolla2937@gmail.com>
2026-02-26 17:03:22 +0600

    added type writer

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,11 +1,24 @@
+import Typewriter from "typewriter-effect";
+
 const Banner = () => {
+
   return (
     <section className="bg-gray-500 py-20">
       <div className="max-w-6xl mx-auto px-6 text-center">
 
         <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-6">
-          Welcome to Our Store
+          <Typewriter
+            options={{
+              strings: ["Welcome to Our Store 😌", "React Lover 🔥", "UI Designer ✨"],
+              autoStart: true,
+              loop: true,
+              delay: 200,        // typing speed
+              deleteSpeed: 200,
+              cursor: "🔥",
+            }}
+          />
         </h1>
+        
 
         <p className="text-gray-600 text-lg mb-8">
           Find the best products for you 😌✨
